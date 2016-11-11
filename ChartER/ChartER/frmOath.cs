9489f5364ff7c8a65bbbb3f1d7ca88b4fa115989
PldAce <acarr009@fiu.edu>
2016-11-10 19:12:11 -0500

    Minor update to oathdialog

--- ChartER/ChartER/frmOath.cs
+++ ChartER/ChartER/frmOath.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,37 @@ using System.Windows.Forms;
 
 namespace ChartER
 {
-    public partial class frmOath : Form
+    public partial class frmOath : TextDesigner.BaseDialogForm
     {
         public frmOath()
         {
             InitializeComponent();
+        }
+
+        private void DrawBackground()
+        {
+
+        }
+
+        /*
+         * Dynamically draw a background based on a PathGradientBrush
+         */
+        private void frmOath_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            using (GraphicsPath bgBrushPath = new GraphicsPath())
+            {
+                bgBrushPath.AddRectangle(new Rectangle(0, 0, 15, 15));
+
+                using (PathGradientBrush bgBrush = new PathGradientBrush(bgBrushPath))
+                {
+                    bgBrush.WrapMode = WrapMode.Tile;
+                    bgBrush.CenterColor = Color.White;
+                    bgBrush.SurroundColors = new Color[] { Color.LightGray };
+                    g.FillRectangle(bgBrush, this.ClientRectangle);
+                }
+            }
         }
     }
 }
